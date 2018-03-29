5376c110eaf4f6231966a9deaefb141076b31049
eliburkart <eliburkart@gmail.com>
2018-03-29 15:33:47 -0500

    added js file

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+var teacherName = "Teacher: Sally Jones";
+var deptName = "Department: Physics";
+var rating = "Rating: 3.4, 5.0, 4.2";
+var avgRating = ((3.4 + 5.0 + 4.2)/3).toFixed();
+
+
+console.log(teacherName);
+console.log(deptName);
+console.log(rating);
+console.log("Average Rating: "+ avgRating);
