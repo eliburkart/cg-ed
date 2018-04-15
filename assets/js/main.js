a67bb6bc1470a66910a15c11e46b677610e0c644
eli burkart <eliburkart@gmail.com>
2018-04-15 12:04:31 -0500

    JS Assignment1 complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,17 @@
 var teacherName = "Teacher: Sally Jones";
 var deptName = "Department: Physics";
 var rating = "Rating: 3.4, 5.0, 4.2";
-var avgRating = ((3.4 + 5.0 + 4.2)/3).toFixed();
-
+var avgRating = ((3.4 + 5.0 + 4.2)/3).toFixed(1);
+var students = "Jessica Jones, Morris Moss, Bernard Black, Manny Bianco, Hermionie Grainger"
+var courses = "History, Art, Communications, Magic"
+var GPA = "3.2, 4.0, 3.8, 4.0, 2"
+var semester = "Winter 2017, Summer 2018, Fall 2018"
 
 console.log(teacherName);
 console.log(deptName);
 console.log(rating);
 console.log("Average Rating: "+ avgRating);
+
+console.log("Students: " + students);
+console.log("Semesters: " + semester);
+console.log("Courses: " + courses);
