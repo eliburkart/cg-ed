0bb77dac8ac378447040db5fea683da77ac81a94
eliburkart <eliburkart@gmail.com>
2018-04-16 15:23:44 -0500

    more work on assignment 2 of javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ var students = ['Jessica Jones', 'Morris Moss', 'Bernard Black', 'Manny Bianco',
 var courses = ['History', 'Art', 'Communications', 'Magic'];
 var GPA = [3.2, 4.0, 3.8, 4.0, 2];
 var semester = ['Winter 2017', 'Summer 2018', 'Fall 2018'];
+var courseDept = [['Roman History','History'], ['Successful Negotiation', 'Communication']]
 
 console.log("Teacher: " + teacherName);
 console.log("Department: " + deptName);
@@ -17,37 +18,36 @@ console.log("Students: " + students);
 console.log("Semesters: " + semester);
 console.log("Courses: " + courses);
 
+
 //STEP 2: Turn teacher’s avg rating calculation into a function
+
 function getAvgRating() {
-  return (rating[0] + rating[1] + rating[2]) / 3;
+  // rating.forEach(function(rating, i)){
+  //   console.log(i + ':' + rating);
+  // }
+  // rating.push(userReview);
+    for (var i = 0; i < rating.length; i++) {
+    var newRating = rating[i] + rating[i];
+   }
 }
+
 console.log(getAvgRating());
-
 //STEP 3: Create an add teacher rating function
-function addTeacherRating() {
-  rating.push(4);
-
-}
-  addTeacherRating();
-  console.log(rating);
+// function addTeacherRating() {
+//   rating.push(userReview);
+// }
+  // addTeacherRating(userReview);
+  // console.log(rating);
 
 //create a prompt that asks the user to review a teacher
-
-//“We would like for you to review . Please enter a rating between 0.0 - 5.0?"
-//1) check that the user entered a number between 0.0 and 5.0…
-//2) if they did not, prompt them again…
 //3) if they did, add the rating value to the teacher’s rating array
 //4) AND alert back to the user… “Thanks for you review! <teacher’s name> average rating is now ."
 
-console.log('We would like for you to review . Please enter a rating between 0.0 - 5.0');
-//
-function reviewTeacher() {
+var userReview = prompt('We would like for you to review your teacher. Please enter a rating between 0 - 5');
 
-    if  rating > 0.0 && rating <= 5.0;
-    addTeacherRating(4);
-    console.log('Thanks for you review! <teacher’s name> average rating is now ' + getAvgRating())
+    if  (userReview > 0 && userReview <= 5) {
+    alert('Thanks for you review! Your teacher now has an average rating of ' + getAvgRating());
 } else {
-   console.log('Please enter a rating between 0.0 - 5.0')
-
+   prompt('Please enter a rating between 0 - 5');
 
 }
